96a18db15bfb08111406ef7c001ea4bf87f804e6
Ekaterina Petrenko <petrenko.e.a@edu.mirea.ru>
2026-05-04 16:33:48 +0300

    docosaurus

---
+++ my-docs/sidebars.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  tutorialSidebar: [
+    "intro",
+    
+    
+    
+    
+    
+    
+    
+    {
+      type: "category",
+      label: "Шаблоны",
+      items: [
+        "templates/api-template",
+        "templates/architecture-template",
+        "templates/algorithm-template"
+],
+    },
+  ],
+};
